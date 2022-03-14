5ea8e98b60342b7fb8e4d273ba5da89886e51c8b
MADROCHA <madlarocha@gmail.com>
2022-03-14 14:43:38 +0100

    commit

--- js/movies.js
+++ js/movies.js
@@ -1,4 +1,4 @@
-const requestURL = "peliculas.json";
+const requestURL = "./json/peliculas.json";
 
 async function fetchMoviesJSON(){
     const response = await fetch(requestURL);
@@ -7,8 +7,28 @@ async function fetchMoviesJSON(){
 }
 fetchMoviesJSON().then(movies => {
     for (let index = 0; index < movies.peliculas.length; index++){
+        const movieSection = document.getElementById('movieSection');
+        let title = movies.peliculas[index].nombre;
+        let director = movies.peliculas[index].director;
+        let category = movies.peliculas[index].clasificacion;
+        let image = movies.peliculas[index].imagen;
         
+
+        movieSection.innerHTML+=`
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${image}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${director}</p>
+                <p class="card-text">${category}</p>
+
+                
+            </div>
+            </div>
+
+        `
+    }    
     
 
-}
+})
     
