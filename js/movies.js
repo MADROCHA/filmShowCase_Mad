1d49c937ef64c9abeee7b723b12f29849fcab850
MADROCHA <madlarocha@gmail.com>
2022-08-05 18:20:26 +0200

    firstCommit_filmShowCase_1.0

--- js/movies.js
+++ js/movies.js
@@ -14,21 +14,81 @@ fetchMoviesJSON().then(movies => {
         let image = movies.peliculas[index].imagen;
         
 
-        movieSection.innerHTML+=`
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="${image}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-                <p class="card-text">${director}</p>
-                <p class="card-text">${category}</p>
+        filmSection.innerHTML+=`
+    <li class="flex  w-full h-full items-center  bg-colorLightGray">
+        
+        
+            <img class="w-1/3  flex " src="${image}" alt="Film Visual Media cap">
+        
+        
+            <div class="w-full flex flex-col px-2.5  ">
+                    
+            
+                    <section class=" flex w-full    	">
+                        <div class="flex ">     
+                            <p class="   ">
+                            ${title}
+                            </p>
+                        </div>     
+                        <div class="flex">
+                            <p>
+                            4
+                            </p>
+                            <p>
+                            /
+                            </p>
+                            <p>
+                            5
+                            </p> 
+                        </div> 
+                    </section>
 
+
+                            
+                    <section class="flex flex-col     ">
+                        <div class="flex gap-[10px]">
+                            <p class=" ">
+                                Genre :
+                            </p>
+                            <p class="">
+                                ${category}
+                            </p>
+                        </div> 
+                        <div class="flex gap-[10px]">
+                            <p class=" ">
+                                Dir :
+                            </p>
+                            <p>
+                                ${director}
+                            </p>
+                        </div> 
+                    </section>
+                    
+                    <div class="flex     border-2 border-colorPurpleLight  "> 
+                        <a href="" 
+                        class="flex py-4  px-8 rounded-[4px] text-xl border-2 text-colorPurpleLight border-colorPurpleLight ">
+                            see more
+                        </a>
+                    </div> 
+            </div> 
+        
                 
-            </div>
-            </div>
+                    
+        
+    </li> 
+                
 
-        `
-    }    
-    
+            
 
+        
+    `  }    
 })
     
+/*             <div class="" style="width: 18rem;">
+                <img class="" src="${image}" alt="Card image cap">
+                <div class="">
+                    <h5 class="text-3xl underline">${title}</h5>
+                    <p class="">${director}</p>
+                    <p class="">${category}</p>
+                </div>
+            </div> */
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "*.{html,js}",
+
+  ],
+  theme: {
+    colors: {
+      'colorLightChampagne': '#F6D8AE',
+      'colorLightGray': '#EEEEEE',
+      'colorBlueDark': '#3D5A6C',
+      'colorPurpleDark': '#1F0322',
+      'colorPurpleLight': '#DA4167',
+      'colorblueLight': '#93C6D6',
+    
+    },
+    fontFamily: {
+      'mont': [ 'Montserrat', 'sans-serif'],
+      'over': [ 'Overpass', 'sans-serif']
+    },
+  },
+  extend: {
+  },
+  plugins: [],
+}
